d213e70816269735fa3a82773a20e7669198778c
ilyamantach <imantach@gmail.com>
2018-05-16 21:01:21 -0500

    Added my project

---
+++ SortingListOfIntegers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SortingListOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sortingList = new List<int> { 1, 3, 2, 8, 5, 3, 4 };
+            sortingList.Sort();
+            foreach (int x in sortingList)
+            {
+                Write(x);
+            }
+            ReadKey();
+        }
+    }
+}
